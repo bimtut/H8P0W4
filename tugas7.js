4434a26c0c2cbca6edd95ff37b76c38952b4fee7
Bima Ketut Ramuna Putra <37969013+bimtut@users.noreply.github.com>
2018-12-14 19:19:56 +0700

    urutkan abjda pake sort

--- tugas7.js
+++ tugas7.js
@@ -2,8 +2,6 @@
 Diberikan sebuah function urutkanAbjad(str) yang menerima satu parameter berupa string. Function tersebut akan mengembalikan string baru yang telah diubah urutan abjadnya dari a hingga z. Contohnya, halo akan menjadi ahlo. Kamu tidak perlu memikirkan apabila ada simbol ataupun angka didalam string tersebut.
 
 Code*/
-
-
 function urutkanAbjad(str) {
   var arr = str.split('');
   arr = arr.sort()
